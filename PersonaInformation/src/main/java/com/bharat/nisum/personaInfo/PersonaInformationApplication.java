a4449646d1d5aae88adca6680949c10446d9420a
Bharat Goswami <Bharat.Goswami@avanza.pk>
2019-11-13 21:12:13 +0500

    PersonaInfoSystem Release Setup

---
+++ PersonaInformation/src/main/java/com/bharat/nisum/personaInfo/PersonaInformationApplication.java
@@ -0,0 +1,13 @@
+package com.bharat.nisum.personaInfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonaInformationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonaInformationApplication.class, args);
+	}
+
+}
---
+++ PersonaInformation/src/test/java/com/bharat/nisum/personaInfo/PersonaInformationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bharat.nisum.personaInfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonaInformationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
